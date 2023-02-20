d8b64981a174034bf5f6929c1a98416bc1ad0ce7
John Wilker <johnwilker2@gmail.com>
2023-02-20 18:00:26 -0300

    feedbackWidgetWeb finished

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -10,7 +10,6 @@ export function FeedbackTypeStep({onFeedbackTypeChanged}: FeedbackTypeStepProps)
     <>
       <header>
         <span className="text-xl leading-6">Deixe seu feedback</span>
-
         <CloseButton />
       </header>
 
@@ -20,8 +19,8 @@ export function FeedbackTypeStep({onFeedbackTypeChanged}: FeedbackTypeStepProps)
             <button
             key={key}
             className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 felx flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-            onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
             type="button"
+            onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
             >
               <img src={value.image.source} alt={value.image.alt} />
               <span>{value.title}</span>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,13 @@ module.exports = {
     extend: {
       colors: {
         brand: {
-          500: '#8257e6',
+          300: '#996DFF',
+          500: '#8257E6',
         }
-      }
+      },
+      borderRadius: {
+        md: '4px',
+      },
     },
   },
   plugins: [
